b289bd6d3e759cb00b4350f9263736a7a32971dd
VladDobychin <146421841+VladDobychin@users.noreply.github.com>
2024-10-04 13:58:55 +0200

    Task-6

---
+++ Task-6/README.md
@@ -0,0 +1,3 @@
+## Instructions
+
+#### [Task 6 - Saving notes to local storage](https://docs.google.com/document/d/14Zyp6xjgn5STvWVLLsK1wmwl6hQVSGYY5PIVYKb-3I0/edit?usp=sharing)
---
+++ Task-6/solution/main.js
@@ -0,0 +1,63 @@
+let notes = [];
+
+function renderNotes() {
+    const notesList = document.getElementById('notes-list');
+    notesList.innerHTML = '';
+
+    notes.forEach((note, index) => {
+        const noteItem = document.createElement('li');
+
+        const noteName = document.createElement('strong');
+        noteName.textContent = note.name;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.style.marginLeft = '10px';
+        deleteButton.addEventListener('click', () => {
+            deleteNote(index);
+        })
+
+        noteItem.appendChild(noteName);
+        noteItem.appendChild(deleteButton);
+        notesList.appendChild(noteItem);
+    });
+}
+
+function deleteNote(index) {
+    notes.splice(index, 1);
+    localStorage.setItem('notes', JSON.stringify(notes));
+    renderNotes();
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const noteName = document.getElementById('note-name-input');
+    const noteContent = document.getElementById('note-content-input');
+    const newNote = {
+        name: noteName.value,
+        content: noteContent.value
+    };
+
+    if (newNote.name !== '' && newNote.content !== '') {
+        notes.push(newNote);
+
+        localStorage.setItem('notes', JSON.stringify(notes));
+
+        noteName.value = '';
+        noteContent.value = '';
+        renderNotes();
+    }
+}
+
+function loadNotesFromStorage() {
+    const savedNotes = localStorage.getItem('notes');
+    if (savedNotes) {
+        console.log('Notes found');
+        notes = JSON.parse(savedNotes);
+        renderNotes();
+    }
+}
+
+document.getElementById('note-form').addEventListener('submit', handleFormSubmit);
+document.addEventListener('DOMContentLoaded', loadNotesFromStorage);
